1cce548425f33d5465e8d6386702269c4621c8c7
Gabriela Munoz Hdz <gabriela_mh_1993@yahoo.com>
2025-04-08 19:31:57 -0600

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from "./task-list/task-list.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [TaskListComponent],
 })
 export class AppComponent {
-  title = 'angular-testing-project';
+  title = 'Lista de actividades';
 
   addTask(task: string) {
 
